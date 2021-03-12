a15f565a0642b3712f5b107e412f9021e45d2c66
ally-petitt <allypetitt@gmail.com>
2021-03-11 18:59:40 -0800

    Can now sort input into an array in preparation for the solving algorithm

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-$(function () {});
-
+//Only allow 1 number to be entered
 $("input").on("input", (e) => {
   let input = e.target;
   if (input.value.length > 1) {
     input.value = input.value.slice(1);
+  }
+});
+
+const board = [];
+
+// adds values to board array
+$(".submit").click(() => {
+  for (var i = 0; i < $("input").length; i++) {
+    const currentBlock = Math.floor(i / 9);
+    const localRow = Math.floor(i / 3);
+    const currentRow = (localRow % 3) + Math.floor(currentBlock / 3) * 3;
+    board[currentRow].push($("input")[i].value);
   }
 });
